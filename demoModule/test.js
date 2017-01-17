df4c6dbedfb8e0ec31d3728f4738517aed118b89
zhaoky <623212389@qq.com>
2017-01-17 16:12:46 +0800

    demoModule

---
+++ demoModule/test.js
@@ -0,0 +1,43 @@
+/**
+ * Created by zhaoky on 2017/1/17.
+ */
+angular
+	.module("demoModule", [
+		"module1",
+		"module2",
+		"module3",
+		"module4"
+	]);
+
+angular
+	.module("module1", [])
+	.controller("controller1", function () {
+		console.log("controller1");
+	});
+
+angular
+	.module("module2", [])
+	.service("service2", function () {
+		this.name = "zky";
+	})
+	.controller("controller2", function () {
+		console.log("controller2");
+	});
+
+angular
+	.module("module3", [])
+	.controller("controller3", [
+		"service2",
+		function (service2) {
+			console.log("controller3", service2.name);
+		}]);
+
+
+angular
+	.module("module4", [])
+	.controller("controller4", function () {
+		console.log("controller4");
+	});
+window.onload=function(){
+	angular.bootstrap(document, ["demoModule"]);
+};
